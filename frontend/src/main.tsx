089e926f6546c65c271ec14c1052bc70141b4d2e
tqcss <134121905+tqcss@users.noreply.github.com>
2026-08-23 21:18:05 +0800

    chore: wire tanstack query provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
+
+import { queryClient } from "./lib/query-client";
 import { router } from "./router";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
